48c9173ddb7a326fc6ce6cc3f7ab7a598462926d
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-06-28 17:06:43 +0400

    Renaming Fixed Spawn Timer

--- 2D3D/Assets/Scripts/RamdomSpanwer.cs
+++ 2D3D/Assets/Scripts/RamdomSpanwer.cs
@@ -8,18 +8,18 @@ public class RamdomSpanwer : MonoBehaviour
     public GameObject[] enemyPrefabs;
 
     public float SpawnTimer = 2.0f;
-    public float Timer;
+    public float FixedSpawnTimer;
 
     // Update is called once per frame
     void Update()
     {
         SpawnTimer -= Time.deltaTime;
-        Timer = 2.0f;
+        FixedSpawnTimer = 2.0f;
 
         //Spawning enemies
         if (SpawnTimer < 0.0f)
         {
-            SpawnTimer = Timer;
+            SpawnTimer = FixedSpawnTimer;
 
             int EnemySpawn = Random.Range(0, enemyPrefabs.Length);
             int SpawnLocation = Random.Range(0, spawnPoints.Length);

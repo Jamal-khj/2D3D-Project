097f4ad746fba314d14d8eab9aabaee4c0ee60e9
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-06-28 17:05:20 +0400

    Timer made public for tweaking

--- 2D3D/Assets/Scripts/RamdomSpanwer.cs
+++ 2D3D/Assets/Scripts/RamdomSpanwer.cs
@@ -7,17 +7,19 @@ public class RamdomSpanwer : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject[] enemyPrefabs;
 
-    public float SpawnTimer = 0.0f;
+    public float SpawnTimer = 2.0f;
+    public float Timer;
 
     // Update is called once per frame
     void Update()
     {
-        SpawnTimer += Time.deltaTime;
+        SpawnTimer -= Time.deltaTime;
+        Timer = 2.0f;
 
         //Spawning enemies
-        if (SpawnTimer > 2.0f)
+        if (SpawnTimer < 0.0f)
         {
-            SpawnTimer = 0.0f;
+            SpawnTimer = Timer;
 
             int EnemySpawn = Random.Range(0, enemyPrefabs.Length);
             int SpawnLocation = Random.Range(0, spawnPoints.Length);

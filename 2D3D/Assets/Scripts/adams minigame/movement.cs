d6ebe0c86ecd48ee0f266437eab10aa578165ef4
adam mekkaoui <a.mekkaoui@student.saedubai,com>
2023-07-03 21:10:54 +0400

    added wall jumping script but its not working

--- 2D3D/Assets/Scripts/adams minigame/movement.cs
+++ 2D3D/Assets/Scripts/adams minigame/movement.cs
@@ -10,12 +10,15 @@ public class movement : MonoBehaviour
     private float horizontal;
     private float jumpingpower;
     private bool isfacingright = true;
-
     private bool doublejump;
+    private bool iswallsliding = true;
+    private float wallslidingspeed = 2f;
 
     [SerializeField] Rigidbody2D rb2d;
     [SerializeField] private Transform groundcheck;
     [SerializeField] private LayerMask groundlayer;
+    [SerializeField] private Transform wallcheck;
+    [SerializeField] private LayerMask walllayer;
     void Start()
     {
       
@@ -47,7 +50,8 @@ public class movement : MonoBehaviour
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
         }
-       
+
+        WallSlide();
         flip();
     }
 
@@ -58,6 +62,24 @@ public class movement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundlayer);
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallcheck.position, 0.2f,walllayer);
+    }
+
+    private void WallSlide()
+    {
+        if (IsWalled() && !IsGrounded() && horizontal != 0f)
+        {
+            iswallsliding= true;
+            rb2d.velocity = new Vector2(rb2d.velocity.x,Mathf.Clamp(rb2d.velocity.y - wallslidingspeed, float.MaxValue));
+        }
+        else
+        {
+            iswallsliding= false;
+        }
     }
     private void flip()
     {

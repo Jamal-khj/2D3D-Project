555755ae8ca29f24261af09732fa39312735b909
adam mekkaoui <a.mekkaoui@student.saedubai,com>
2023-07-04 21:58:02 +0400

    cleaned up script

--- 2D3D/Assets/Scripts/wingame.cs
+++ 2D3D/Assets/Scripts/wingame.cs
@@ -10,11 +10,6 @@ public class wingame : MonoBehaviour
         winmessage.SetActive(false);
     }
 
-    
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "player")

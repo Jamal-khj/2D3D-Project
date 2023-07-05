a3622d0fea1a19ef4203e3427296eacda4796aba
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-07-05 18:40:04 +0400

    Fixed exit arcade script

--- 2D3D/Assets/Scripts/3D Scripts/ExitArcade.cs
+++ 2D3D/Assets/Scripts/3D Scripts/ExitArcade.cs
@@ -5,23 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class ExitArcade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             SceneManager.LoadScene("Main Menu");
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 }
